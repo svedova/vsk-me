82e1776cae43c41f18fe4813d3b2bac4af53fcfc
Savas Vedova <savas.vedova@gmail.com>
2017-10-20 18:53:42 +0200

    Configure testing

--- jest.config.js
+++ jest.config.js
@@ -1,26 +1,26 @@
 module.exports = {
   "collectCoverageFrom": [
-    "src/**/*.{js,jsx}"
+    "src/**/*.{js,jsx}",
   ],
   "setupFiles": [
-    "<rootDir>/config/polyfills.js"
+    "<rootDir>/config/polyfills.js",
   ],
   "testMatch": [
     "<rootDir>/src/**/__tests__/**/*.js?(x)",
-    "<rootDir>/src/**/?(*.)(spec|test).js?(x)"
+    "<rootDir>/src/**/?(*.)(spec|test).js?(x)",
   ],
   "testEnvironment": "node",
   "testURL": "http://localhost",
   "transform": {
     "^.+\\.(js|jsx)$": "<rootDir>/node_modules/babel-jest",
     "^.+\\.css$": "<rootDir>/config/jest/cssTransform.js",
-    "^(?!.*\\.(js|jsx|css|json)$)": "<rootDir>/config/jest/fileTransform.js"
+    "^(?!.*\\.(js|jsx|css|json)$)": "<rootDir>/config/jest/fileTransform.js",
   },
   "transformIgnorePatterns": [
-    "[/\\\\]node_modules[/\\\\].+\\.(js|jsx)$"
+    "[/\\\\]node_modules[/\\\\].+\\.(js|jsx)$",
   ],
   "moduleNameMapper": {
-    "^react-native$": "react-native-web"
+    "^react-native$": "react-native-web",
   },
   "moduleFileExtensions": [
     "web.js",
@@ -28,6 +28,10 @@ module.exports = {
     "json",
     "web.jsx",
     "jsx",
-    "node"
-  ]
+    "node",
+  ],
+  "setupFiles": [
+    "raf/polyfill",
+  ],
+  "setupTestFrameworkScriptFile": "<rootDir>/test/setup.js",
 }

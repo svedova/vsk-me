96a3378a9eb9f09bddf1b5abb3d49e2641d3e2e2
Savas Vedova <savas.vedova@gmail.com>
2017-10-29 12:13:32 +0100

    Remove service worker temporarily

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import CloudFlash from "cloudflash.io-client"
-import registerServiceWorker from './registerServiceWorker';
+// import registerServiceWorker from './registerServiceWorker';
 
 // This is required
 window.CloudFlash = CloudFlash
 
 ReactDOM.hydrate(<App />, document.getElementById('root'));
-registerServiceWorker();
+// registerServiceWorker();

aae7aba9f94ffeb915f638a86a3f3181c0271c4c
Savas Vedova <savas.vedova@gmail.com>
2017-12-07 22:03:08 +0100

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
-ssh ubuntu@vsk "rm -rf /var/www/vsk-me-react && mkdir /var/www/vsk-me-react" && scp -r build/* ubuntu@vsk:/var/www/vsk-me-react
+npm run build
+ssh ubuntu@vsk "rm -rf /var/www/vsk-me-react && mkdir /var/www/vsk-me-react"
+scp -r build/* ubuntu@vsk:/var/www/vsk-me-react

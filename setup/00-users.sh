4dd6733483f96615ce07c80a2dc1ea219f959ca1
Savas Vedova <savas@vsk.me>
2018-08-13 00:37:15 +0200

    Fix some commands

--- setup/00-users.sh
+++ setup/00-users.sh
@@ -3,15 +3,12 @@ echo -n "Type the name of the user and press [ENTER]:"
 read name
 
 sudo adduser ${name} --disabled-password
-sudo su - ${name}
-cd && mkdir .ssh && chmod 700 .ssh/
-touch .ssh/authorized_keys
-chmod 600 .ssh/authorized_keys
+sudo su ${name} -c "cd && mkdir .ssh && chmod 700 .ssh/ && touch .ssh/authorized_keys && chmod 600 .ssh/authorized_keys"
 
 echo -n "Paste the public SSH key and press [ENTER]:"
 read key
 
-echo ${key} > .ssh/authorized_keys
+sudo su ${name} -c "echo ${key} > .ssh/authorized_keys"
 
 echo -n "Add a new line for the user to emit password"
 sudo visudo -f /etc/sudoers.d/90-cloud-init-users

72e6433716bc0aecc6d106409c2a9db0259e95c4
Savas Vedova <savas@vsk.me>
2018-12-18 14:22:02 +0100

    Normalize exports

--- src/renderer.js
+++ src/renderer.js
@@ -2,25 +2,28 @@ import React from "react";
 import { StaticRouter } from "react-router-dom";
 import Helmet from "react-helmet";
 import App from "./App";
+import routes from "./routes/server";
 
-export { default as routes } from "./routes/server";
+export default {
+  routes,
 
-export default async (context, render) => {
-  if (context.route && context.route.setup) {
-    await context.route.setup(context);
+  renderer: async (context, render) => {
+    if (context.route && context.route.setup) {
+      await context.route.setup(context);
+    }
+
+    const router = props => (
+      <StaticRouter
+        {...props}
+        context={context}
+        location={context.request.path}
+      />
+    );
+
+    const body = render(<App Router={router} />);
+    const data = Helmet.renderStatic();
+    const head = Object.keys(data).map(k => data[k].toString()).join(""); // prettier-ignore
+
+    return { body, head, headers: {} };
   }
-
-  const router = props => (
-    <StaticRouter
-      {...props}
-      context={context}
-      location={context.request.path}
-    />
-  );
-
-  const body = render(<App Router={router} />);
-  const data = Helmet.renderStatic();
-  const head = Object.keys(data).map(k => data[k].toString()).join(""); // prettier-ignore
-
-  return { body, head, headers: {} };
 };

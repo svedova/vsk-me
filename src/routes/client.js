80d865e377fa04c57a22f0706028ddecf577d35e
Savas Vedova <savas@vsk.me>
2018-12-18 12:38:03 +0100

    Fix paths

--- src/routes/client.js
+++ src/routes/client.js
@@ -1,7 +1,7 @@
-import Home from "./components/Home";
-import CV from "./components/CV";
-import About from "./components/About";
-import Contact from "./components/Contact";
+import Home from "../components/Home";
+import CV from "../components/CV";
+import About from "../components/About";
+import Contact from "../components/Contact";
 
 export const routes = [
   { path: "/", component: Home, exact: true },

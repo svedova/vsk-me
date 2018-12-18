c4305c8fdb7074f6c53e20ddedcc0c76719d674a
Savas Vedova <savas@vsk.me>
2018-12-18 15:21:48 +0100

    Concat without array

--- src/routes/server.js
+++ src/routes/server.js
@@ -1,14 +1,12 @@
 import clientRoutes from "./client";
 import robots from "../robots.txt";
 
-export default clientRoutes.concat([
-  {
-    path: "/robots.txt",
-    setup: async () => ({
-      headers: {
-        "Content-Type": "text/plain; charset=utf-8"
-      },
-      body: robots
-    })
-  }
-]);
+export default clientRoutes.concat({
+  path: "/robots.txt",
+  setup: async () => ({
+    headers: {
+      "Content-Type": "text/plain; charset=utf-8"
+    },
+    body: robots
+  })
+});

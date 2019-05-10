7409858f1f191d6cec70d5e78d8f44779eed4200
Savas Vedova <savas@vsk.me>
2019-05-10 11:28:38 +0200

    Add yandex verification

--- packages/vsk.me/src/routes/server.js
+++ packages/vsk.me/src/routes/server.js
@@ -5,12 +5,18 @@ User-agent: *
 Allow: /
 `;
 
-export default clientRoutes.concat({
-  path: "/robots.txt",
-  setup: async () => ({
-    headers: {
-      "Content-Type": "text/plain; charset=utf-8"
-    },
-    body: robotsTxt.trim()
-  })
-});
+export default clientRoutes.concat(
+  {
+    path: "/robots.txt",
+    setup: async () => ({
+      headers: {
+        "Content-Type": "text/plain; charset=utf-8"
+      },
+      body: robotsTxt.trim()
+    })
+  },
+  {
+    path: "/yandex_1944487f2bf42ca9.html",
+    setup: async () => ({ body: `Verification: 1944487f2bf42ca9` })
+  }
+);

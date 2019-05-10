b2d800ff2b034e3b307d59aeafa9517ba3a371c7
Savas Vedova <savas@vsk.me>
2019-05-10 11:33:54 +0200

    Tell stormkit not to interpolate

--- packages/vsk.me/src/routes/server.js
+++ packages/vsk.me/src/routes/server.js
@@ -5,6 +5,15 @@ User-agent: *
 Allow: /
 `;
 
+const yandexVerification = `
+<html>
+  <head>
+      <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  </head>
+  <body>Verification: 1944487f2bf42ca9</body>
+</html>
+`;
+
 export default clientRoutes.concat(
   {
     path: "/robots.txt",
@@ -17,6 +26,14 @@ export default clientRoutes.concat(
   },
   {
     path: "/yandex_1944487f2bf42ca9.html",
-    setup: async () => ({ body: `Verification: 1944487f2bf42ca9` })
+    setup: async () => ({
+      headers: {
+        "Content-Type": "text/html; charset=utf-8"
+      },
+      body: yandexVerification.trim(),
+      options: {
+        interpolate: false
+      }
+    })
   }
 );

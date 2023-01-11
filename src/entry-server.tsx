1932a24e3a652f666f4f03b1be5b5ee6fb090b66
Savas Vedova <savas@stormkit.io>
2023-01-11 17:09:54 +0300

    fix: typo

--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -57,7 +57,7 @@ const renderHead = (overwrite?: OG) => {
       <meta name="description" content="${tags.description}" />
       <meta charset="utf-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <meta property="og:title" content="Savas Vedova | {tags.title}" />
+      <meta property="og:title" content="Savas Vedova | ${tags.title}" />
       <meta property="og:url" content="https://vsk.me/${path}" />
       <meta property="og:description" content="${tags.description}" />
       <meta name="twitter:card" content="${tags.twitter!.card}" />
